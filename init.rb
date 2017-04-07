fc53fcb287d4b1d7dfaff7285230739a88899df3
COLA@Redmine.local <carloskar.larsson@gmail.com>
2017-04-07 10:18:11 +0200

    Initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_project_email do
+  name 'Redmine Project Email plugin'
+  author 'Carl-Oskar Larsson'
+  description 'Shows the project email on the project overview page.'
+  version '0.0.1'
+  url 'n/a'
+  author_url 'n/a'
+end
