06954c3b896891cbf96f858e99a36de025a024cb
COLA@Redmine.local <carloskar.larsson@gmail.com>
2017-04-07 12:04:18 +0200

    Added settings to enable/disable project email

--- init.rb
+++ init.rb
@@ -5,4 +5,10 @@ Redmine::Plugin.register :redmine_project_email do
   version '0.0.1'
   url 'n/a'
   author_url 'n/a'
+  
+  settings  partial: 'settings/redmine_project_email',
+            default:
+            {
+              'show_project_email': false
+            }
 end
